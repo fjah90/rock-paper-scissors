38923f64d943d969c941af6703fbf974b7e98819
Fernando Aponte <fernandoaponte90@gmail.com>
2022-01-04 05:17:22 -0400

    Modificación del juego implementacion: *Reglas The Big Bang Theory para jugar
    *Si un jugador llega a 3 rondas ganadas, la partida finalizará.

--- src/Console/GameCommand.php
+++ src/Console/GameCommand.php
@@ -8,30 +8,59 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class GameCommand extends Command
 {
+    private $players = [];
+    private $player_name;
+    private $theBigBangTheory = false;
+    // Weapons available
+    private $weapons = [
+            0 => 'Tijeras',
+            1 => 'Piedra',
+            2 => 'Papel'
+    ];
+    // Rules to win
+    private $rules = [
+        0 => 2,
+        1 => 0,
+        2 => 1
+    ];
+    //Config
+    private $round = 1;
+    private $max_round = 5;
+    private $maxWins = 3;
+
     /**
      * Configure the command options.
      *
      * @return void
      */
-    protected function configure()
+
+    protected function configure(): void
     {
         $this->setName('game')
             ->setDescription('New game: you vs computer')
             ->addArgument('name', InputArgument::OPTIONAL, 'what is your name?', 'Player 1');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /**
+     * Execute the console game.
+     *
+     * @return integer
+     */
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $output->write(PHP_EOL . 'Made with ♥ by Uniqoders.' . PHP_EOL . PHP_EOL);
+        $output->write(PHP_EOL . 'Modified by Fernando Aponte 04/01/2022.' . PHP_EOL . PHP_EOL . PHP_EOL);
 
-        $player_name = $input->getArgument('name');
+        $this->player_name = $input->getArgument('name');
 
-        $players = [
+        $this->players = [
             'player' => [
-                'name' => $player_name,
+                'name' => $this->player_name,
                 'stats' => [
                     'draw' => 0,
                     'victory' => 0,
@@ -48,60 +77,61 @@ class GameCommand extends Command
             ]
         ];
 
-        // Weapons available
-        $weapons = [
-            0 => 'Scissors',
-            1 => 'Rock',
-            2 => 'Paper'
-        ];
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('Te gustaria jugar con las reglas "The Big Bang Theory"? y/n '. PHP_EOL, false, '/^(y|j)/i');
+        
+        if ($helper->ask($input, $output, $question)) {
+            $this->theBigBangTheory = true;
+            array_push($this->weapons, 'Lagarto', 'Spock');
+            $this->rules = [
+                0 => [ 0 => 2 ],
+                1 => [ 0 => 3 ],
+                2 => [ 1 => 0 ],
+                3 => [ 1 => 3 ],
+                4 => [ 2 => 1 ],
+                5 => [ 2 => 4 ],
+                6 => [ 3 => 2 ],
+                7 => [ 3 => 4 ],
+                8 => [ 4 => 0 ],
+                9 => [ 4 => 1 ],
+            ];
+        }
 
-        // Rules to win
-        $rules = [
-            0 => 2,
-            1 => 0,
-            2 => 1
-        ];
-
-        $round = 1;
-        $max_round = 5;
-
+        $question = new ConfirmationQuestion('Te gustaria cambiar la cantidad maxima de rondas? y/n '. PHP_EOL, false, '/^(y|j)/i');
         $ask = $this->getHelper('question');
 
         do {
             // User selection
-            $question = new ChoiceQuestion('Please select your weapon', array_values($weapons), 1);
+            $question = new ChoiceQuestion('Please select your weapon', array_values($this->weapons), 1);
             $question->setErrorMessage('Weapon %s is invalid.');
 
             $user_weapon = $ask->ask($input, $output, $question);
-            $output->writeln('You have just selected: ' . $user_weapon);
-            $user_weapon = array_search($user_weapon, $weapons);
+            $output->writeln('Genial! Has seleccionado: ' . $user_weapon);
+            $user_weapon = array_search($user_weapon, $this->weapons);
 
             // Computer selection
-            $computer_weapon = array_rand($weapons);
-            $output->writeln('Computer has just selected: ' . $weapons[$computer_weapon]);
+            $computer_weapon = array_rand($this->weapons);
 
-            if ($rules[$user_weapon] === $computer_weapon) {
-                $players['player']['stats']['victory']++;
-                $players['computer']['stats']['defeat']++;
-
-                $output->writeln($player_name . ' win!');
-            } else if ($rules[$computer_weapon] === $user_weapon) {
-                $players['player']['stats']['defeat']++;
-                $players['computer']['stats']['victory']++;
-
-                $output->writeln('Computer win!');
-            } else {
-                $players['player']['stats']['draw']++;
-                $players['computer']['stats']['draw']++;
-
-                $output->writeln('Draw!');
+            $output->writeln('Tu oponente a selecionado: ' . $this->weapons[$computer_weapon]);
+            $output->writeln('Round: ' .$this->round);
+            if(!$this->theBigBangTheory){
+                // Result with default rules
+                $this->setResult($user_weapon, $computer_weapon, $output);
+            }else{
+                // Result with theBigBangTheory rules
+                $this->setTBBTResult($user_weapon, $computer_weapon, $output);
             }
 
-            $round++;
-        } while ($round <= $max_round);
+            if($this->players['player']['stats']['victory'] == $this->maxWins || $this->players['computer']['stats']['victory'] == $this->maxWins){
+                break;
+            }else{
+                $this->round++;
+            }
+
+        } while ($this->round <= $this->max_round);
 
         // Display stats
-        $stats = $players;
+        $stats = $this->players;
 
         $stats = array_map(function ($player) {
             return [$player['name'], $player['stats']['victory'], $player['stats']['draw'], $player['stats']['defeat']];
@@ -109,11 +139,76 @@ class GameCommand extends Command
 
         $table = new Table($output);
         $table
-            ->setHeaders(['Player', 'Victory', 'Draw', 'Defeat'])
+            ->setHeaders(['Jugador', 'Rondas ganadas', 'Rondas empatadas', 'Rondas perdidas'])
             ->setRows($stats);
 
         $table->render();
 
         return 0;
     }
+
+    /**
+     * Set Results with the normal rules.
+     * @param integer $user_weapon The selected user weapon
+     * @param integer $computer_weapon The selected computer weapon
+     * @param object  $output The OutputInterface
+     * @return void
+     */
+
+    protected function setResult($user_weapon, $computer_weapon, $output){
+
+        if ($this->rules[$user_weapon] === $computer_weapon) {
+            $this->players['player']['stats']['victory']++;
+            $this->players['computer']['stats']['defeat']++;
+
+            $output->writeln($this->player_name . ' win!');
+        } else if ($this->rules[$computer_weapon] === $user_weapon) {
+            $this->players['player']['stats']['defeat']++;
+            $this->players['computer']['stats']['victory']++;
+
+            $output->writeln('Computer win!');
+        } else {
+            $this->players['player']['stats']['draw']++;
+            $this->players['computer']['stats']['draw']++;
+
+            $output->writeln('Draw!');
+        }
+    }
+
+    /**
+     * Set Results with the big bang theory rules.
+     * @param integer $user_weapon The selected user weapon
+     * @param integer $computer_weapon The selected computer weapon
+     * @param object  $output The OutputInterface
+     * @return void
+     */
+
+    protected function setTBBTResult($user_weapon, $computer_weapon, $output){
+        $empate;
+        foreach ($this->rules as $key => $val) {
+            $empate = false;
+            if (isset($val[$user_weapon]) && $val[$user_weapon] === $computer_weapon) {
+                $this->players['player']['stats']['victory']++;
+                $this->players['computer']['stats']['defeat']++;
+
+                $output->writeln($this->player_name . ' win!');
+                break;
+            } else if (isset($val[$computer_weapon]) && $val[$computer_weapon] === $user_weapon) {
+                $this->players['player']['stats']['defeat']++;
+                $this->players['computer']['stats']['victory']++;
+
+                $output->writeln('Computer win!');
+                break;
+            } else {
+                $empate = true;
+            }
+        }
+        if($empate){
+            $this->players['player']['stats']['draw']++;
+            $this->players['computer']['stats']['draw']++;
+
+            $output->writeln('Draw!');
+        }
+    }
+
 }
