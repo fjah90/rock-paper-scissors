eba112855b12f90fefa9504d41eac5d1363e43b8
Fernando Aponte <fernandoaponte90@gmail.com>
2022-01-07 00:18:33 -0400

    Refactoring GameCommand

--- src/Console/GameCommand.php
+++ src/Console/GameCommand.php
@@ -10,23 +10,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ChoiceQuestion;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
+use Uniqoders\Game\Console\Wepons;
+use Uniqoders\Game\Console\Player;
+use Uniqoders\Game\Console\Rules;
+
 class GameCommand extends Command
 {
-    private $players = [];
+    private $players;
     private $player_name;
-    private $theBigBangTheory = false;
     // Weapons available
-    private $weapons = [
-            0 => 'Tijeras',
-            1 => 'Piedra',
-            2 => 'Papel'
-    ];
+    private $weapons;
+    private $theBigBangTheory = false;
     // Rules to win
-    private $rules = [
-        0 => 2,
-        1 => 0,
-        2 => 1
-    ];
+    private $rules;
     //Config
     private $round = 1;
     private $max_round = 5;
@@ -45,20 +41,22 @@ class GameCommand extends Command
             ->addArgument('name', InputArgument::OPTIONAL, 'what is your name?', 'Player 1');
     }
 
+
     /**
-     * Execute the console game.
+     * Init configuration.
      *
-     * @return integer
+     * @return void
      */
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
+    protected function initConfig($input, $output): void
     {
-        $output->write(PHP_EOL . 'Made with ♥ by Uniqoders.' . PHP_EOL . PHP_EOL);
-        $output->write(PHP_EOL . 'Modified by Fernando Aponte 04/01/2022.' . PHP_EOL . PHP_EOL . PHP_EOL);
 
         $this->player_name = $input->getArgument('name');
+        $this->weapons = new Wepons();
+        $this->rules = new Rules();
+        $this->players = new Players();
 
-        $this->players = [
+        $this->players->setPlayers([
             'player' => [
                 'name' => $this->player_name,
                 'stats' => [
@@ -75,14 +73,15 @@ class GameCommand extends Command
                     'defeat' => 0,
                 ]
             ]
-        ];
+        ]);
 
         $question_theBigBangTheory = $this->customConfrimQuestion('Te gustaria jugar con las reglas "The Big Bang Theory"? y/n ', $input, $output);
 
         if ($question_theBigBangTheory) {
+
             $this->theBigBangTheory = true;
-            array_push($this->weapons, 'Lagarto', 'Spock');
-            $this->rules = [
+
+            $this->rules->setRules([
                 0 => [ 0 => 2 ],
                 1 => [ 0 => 3 ],
                 2 => [ 1 => 0 ],
@@ -93,15 +92,54 @@ class GameCommand extends Command
                 7 => [ 3 => 4 ],
                 8 => [ 4 => 0 ],
                 9 => [ 4 => 1 ],
-            ];
+            ]);
+
+            $this->weapons->setWepons([
+                0 => 'Tijeras',
+                1 => 'Piedra',
+                2 => 'Papel',
+                3 => 'Lagarto',
+                4 => 'Spock'
+            ]);
+        }else{
+
+            $this->rules->setRules([
+                0 => 2,
+                1 => 0,
+                2 => 1
+            ]);
+
+            $this->weapons->setWepons([
+                0 => 'Tijeras',
+                1 => 'Piedra',
+                2 => 'Papel'
+            ]);
         }
 
         $question_upMaxRound = $this->customConfrimQuestion('Te gustaria cambiar la cantidad maxima de rondas? y/n ', $input, $output);
 
         if ($question_upMaxRound)
             $this->max_round = 10;
+    }
+
+    /**
+     * Execute the console game.
+     *
+     * @return integer
+     */
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->write(PHP_EOL . 'Made with ♥ by Uniqoders.' . PHP_EOL . PHP_EOL);
+        $output->write(PHP_EOL . 'Modified by Fernando Aponte 04/01/2022.' . PHP_EOL . PHP_EOL . PHP_EOL);
+
+        //Run init configuration
+        $this->initConfig($input, $output);
 
         do {
+            if($this->players->getVictory('player') === $this->maxWins || $this->players->getVictory('computer') === $this->maxWins)
+                break;
+
             // User selection
             $user_weapon = $this->selectUserWepond($input, $output);
 
@@ -115,9 +153,6 @@ class GameCommand extends Command
             }else{
                 $this->setTBBTResult($user_weapon, $computer_weapon, $output);
             }
-
-            if($this->players['player']['stats']['victory'] === $this->maxWins || $this->players['computer']['stats']['victory'] === $this->maxWins)
-                break;
 
             $this->round++;
 
@@ -139,20 +174,18 @@ class GameCommand extends Command
 
     protected function setResult($user_weapon, $computer_weapon, $output): void
     {
-
-        if ($this->rules[$user_weapon] === $computer_weapon) {
-            $this->players['player']['stats']['victory']++;
-            $this->players['computer']['stats']['defeat']++;
+        if ($this->rules->getRules()[$user_weapon] === $computer_weapon) {
+            $this->players->setVictory('player');
+            $this->players->setDefeat('computer');
 
             $output->writeln($this->player_name . ' win!');
-        } else if ($this->rules[$computer_weapon] === $user_weapon) {
-            $this->players['player']['stats']['defeat']++;
-            $this->players['computer']['stats']['victory']++;
+        } else if ($this->rules->getRules()[$computer_weapon] === $user_weapon) {
+            $this->players->setDefeat('player');
+            $this->players->setVictory('computer');
 
             $output->writeln('Computer win!');
         } else {
-            $this->players['player']['stats']['draw']++;
-            $this->players['computer']['stats']['draw']++;
+            $this->players->setDraw();
 
             $output->writeln('Draw!');
         }
@@ -169,18 +202,17 @@ class GameCommand extends Command
     protected function setTBBTResult($user_weapon, $computer_weapon, $output): void
     {
         $draw;
-        foreach ($this->rules as $key => $val) {
+        foreach ($this->rules->getRules() as $key => $val) {
             $draw = false;
             if (isset($val[$user_weapon]) && $val[$user_weapon] === $computer_weapon){
-                    $this->players['player']['stats']['victory']++;
-                    $this->players['computer']['stats']['defeat']++;
+                $this->players->setVictory('player');
+                $this->players->setDefeat('computer');
 
-                    $output->writeln($this->player_name . ' win!');
-                    break;
+                $output->writeln($this->player_name . ' win!');
+                break;
             }else if(isset($val[$computer_weapon]) && $val[$computer_weapon] === $user_weapon){
-
-                $this->players['player']['stats']['defeat']++;
-                $this->players['computer']['stats']['victory']++;
+                $this->players->setDefeat('player');
+                $this->players->setVictory('computer');
 
                 $output->writeln('Computer win!');
                 break;
@@ -189,8 +221,7 @@ class GameCommand extends Command
             }
         }
         if($draw){
-            $this->players['player']['stats']['draw']++;
-            $this->players['computer']['stats']['draw']++;
+            $this->players->setDraw();
 
             $output->writeln('Draw!');
         }
@@ -221,14 +252,14 @@ class GameCommand extends Command
 
     public function selectUserWepond($input, $output): int
     {
-        $ask = $this->getHelper('question');
-        $question = new ChoiceQuestion('Please select your weapon', array_values($this->weapons), 1);
+        $question = new ChoiceQuestion('Please select your weapon', array_values($this->weapons->getWepons()), 1);
         $question->setErrorMessage('Weapon %s is invalid.');
 
+        $ask = $this->getHelper('question');
         $w = $ask->ask($input, $output, $question);
         $output->writeln('Genial! Has seleccionado: ' . $w);
 
-        $uw = array_search($w, $this->weapons);
+        $uw = array_search($w, $this->weapons->getWepons());
 
         return (int)$uw;
     }
@@ -242,9 +273,9 @@ class GameCommand extends Command
     public function selectComputerWepond($output): int
     {
 
-        $w = array_rand($this->weapons);
+        $w = array_rand($this->weapons->getWepons());
 
-        $output->writeln('Tu oponente a selecionado: ' . $this->weapons[$w]);
+        $output->writeln('Tu oponente a selecionado: ' . $this->weapons->getWeponNameById($w));
 
         return (int)$w;
     }
@@ -257,7 +288,7 @@ class GameCommand extends Command
 
     public function displayStats($output): object
     {
-        $s = $this->players;
+        $s = $this->players->getPlayers();
 
         $s = array_map(function ($p) {
             return [$p['name'], $p['stats']['victory'], $p['stats']['draw'], $p['stats']['defeat']];
