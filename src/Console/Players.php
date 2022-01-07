2811b999e4b44078b7762f2e3bdeba30638fdf27
Fernando Aponte <fernandoaponte90@gmail.com>
2022-01-07 00:17:00 -0400

    Create Class Players

---
+++ src/Console/Players.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Uniqoders\Game\Console;
+
+class Players
+{
+    public $players;
+
+    public function __contruct($players  = []): void
+    {
+        $playerlayers;
+    }
+
+    public function getPlayers(): array
+    {
+        return $this->players;
+    }
+
+    public function setPlayers($players):array
+    {
+        return $this->players = $players;
+    }
+
+    public function getVictory($id): int
+    {
+        return $this->players[$id]['stats']['victory'];
+    }
+
+    public function setVictory($id): void
+    {
+        $this->players[$id]['stats']['victory']++;
+    }
+
+    public function getDefeat($id): int
+    {
+        return $this->players[$id]['stats']['defeat'];
+    }
+
+    public function setDefeat($id): void
+    {
+        $this->players[$id]['stats']['defeat']++;
+    }
+
+    public function setDraw(): void
+    {
+        foreach ($this->players as $key => $item ) {
+            $this->players[$key]['stats']['draw']++;
+        }
+    }
+
+    public function setDrawById($id): void
+    {
+        $this->players[$id]['stats']['draw']++;
+    }
+
+    public function getDraw($id): int
+    {
+        return $this->players[$id]['stats']['draw'];
+    }
+
+}
